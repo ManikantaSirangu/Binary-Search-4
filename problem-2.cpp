0a54e6643d70d674e4d18bdc5a45ad692275673a
manikanta <87496228+ManikantaSirangu@users.noreply.github.com>
2024-04-11 21:13:34 -0700

    Update problem-2.cpp

--- problem-2.cpp
+++ problem-2.cpp
@@ -1,1 +1,15 @@
-
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int n=nums1.size(),m=nums2.size();
+        nums1.insert(nums1.end(),nums2.begin(),nums2.end());
+        sort(nums1.begin(),nums1.end());
+        float f=0;
+        if((n+m)%2==0)
+        {
+            f= nums1[(nums1.size()-1)/2]+nums1[((nums1.size()-1)/2)+1];
+            return f/2;
+        }
+        return nums1[(nums1.size()-1)/2];
+    }
+};
